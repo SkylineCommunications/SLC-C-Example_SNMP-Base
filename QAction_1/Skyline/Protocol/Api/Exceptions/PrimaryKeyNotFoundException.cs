51bc27648d78afb324665bd2bc2f6aa4623418f6
cheeyangK <chee.yang.khoo@skyline.be>
2026-01-12 13:43:45 +0800

    Added table classes

---
+++ QAction_1/Skyline/Protocol/Api/Exceptions/PrimaryKeyNotFoundException.cs
@@ -0,0 +1,43 @@
+﻿namespace Skyline.Protocol.Api.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class PrimaryKeyNotFoundException : Exception
+    {
+        public PrimaryKeyNotFoundException()
+        {
+        }
+
+        public PrimaryKeyNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public PrimaryKeyNotFoundException(int tableId, string primaryKey)
+            : this(BuildMessage(tableId, primaryKey))
+        {
+        }
+
+        public PrimaryKeyNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public PrimaryKeyNotFoundException(int tableId, string primaryKey, Exception innerException)
+            : this(BuildMessage(tableId, primaryKey), innerException)
+        {
+        }
+
+        protected PrimaryKeyNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        private static string BuildMessage(int tableId, string primaryKey)
+        {
+            return $"Table<{tableId}>: Primary key with value '{primaryKey}' not found.";
+        }
+    }
+}
