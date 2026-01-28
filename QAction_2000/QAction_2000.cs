b33af8e5db53c46a5009e5378bcae40953264c83
Simon Vandamme <simon.vandamme@skyline.be>
2026-01-28 13:42:05 +0100

    no longer polling the entire table after Alias set and adding comment to clarify reasoning behind it.

--- QAction_2000/QAction_2000.cs
+++ QAction_2000/QAction_2000.cs
@@ -30,8 +30,10 @@ public class QAction
 						new[] { Parameter.iftablesetinstance, Parameter.Write.iftable_ifadminstatus },
 						new[] { rowKey, value });
 
-					/* Note: We poll the entire table so the bit rate calculation is triggered again using the correct values.
-					 * Polling just the cell or row could lead to wrong calculations. */
+					/* Note: Since changing the admin status has impact on the whole row, we want to poll the whole row rather than just the cell.
+					 * However, if we poll the row via snmpSetAndGet option, counters will be updated but our buffered RatesData will not be updated which would lead to wrong calculations next polling cycle.
+					 * So we keep the snmpSetAndGet only to get the cell for quick update, and we trigger a full table poll to update the RatesData correctly.
+					 */
 					protocol.CheckTrigger(TriggerIfTable);
 					break;
 
@@ -48,7 +50,7 @@ public class QAction
 						new[] { Parameter.ifxtablesetinstance, Parameter.Write.ifxtable_ifalias },
 						new[] { rowKey, value });
 
-					protocol.CheckTrigger(TriggerIfXTable);
+					/* This is just an Alias, won't have impact on rates and counters -> no need to poll the entire table. */
 					break;
 
 				default:
